2660bef80dd1f796537a5891828f85bcff5620d8
hathaiin <hathaipath_in33@hotmail.com>
2018-11-26 22:26:05 +0700

    get data form FB to show display on profile page

--- app/src/main/java/com/cpe327/piko/ActivityHome.java
+++ app/src/main/java/com/cpe327/piko/ActivityHome.java
@@ -1,42 +1,94 @@
 package com.cpe327.piko;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.InputStream;
+import java.net.URL;
+
 public class ActivityHome extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private Button mLogoutBtn;
+    private TextView mTextViewProfileTel;
+    private TextView mTextViewProfileEmail;
+    private TextView mTextViewProfileFB;
+    private TextView mTextViewProfileDOB;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         mAuth = FirebaseAuth.getInstance();
+
+        // Logout Activity (Go to Main Page) //
         mLogoutBtn = (Button)findViewById(R.id.btlogout);
         mLogoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mAuth.signOut();
                 LoginManager.getInstance().logOut();
-                updateUI();
+                Toast.makeText(ActivityHome.this,"You are logged out",Toast.LENGTH_LONG).show();
+                Intent MainIntent = new Intent (ActivityHome.this,MainActivity.class);
+                startActivity(MainIntent);
+                finish();
             }
         });
+
+
+        // Profile Display //
+        //        mTextViewProfileTel = (TextView)findViewById(R.id.tvtel);
+        mImageView = (ImageView)findViewById(R.id.imgProfile);
+        mTextViewProfileEmail = (TextView)findViewById(R.id.tvemail);
+        mTextViewProfileFB = (TextView)findViewById(R.id.tvname);
+//        mTextViewProfileDOB = (TextView) findViewById(R.id.tvDOB);
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        //        mTextViewProfileTel.setText(user.getPhoneNumber());
+
+        new DownloadImageTask().execute(user.getPhotoUrl().toString());
+        mTextViewProfileEmail.setText(user.getEmail());
+        mTextViewProfileFB.setText(user.getDisplayName());
+
+
+
     }
 
-    private void updateUI() {
-        Toast.makeText(ActivityHome.this,"You are logged out",Toast.LENGTH_LONG).show();
-
-        Intent MainIntent = new Intent (ActivityHome.this,MainActivity.class);
-        startActivity(MainIntent);
-        finish();
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            Bitmap mIcon = null;
+            try {
+                InputStream in = new URL(urls[0]).openStream();
+                mIcon = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return mIcon;
+        }
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            if (result != null) {
+                    mImageView.getLayoutParams().width = (getResources().getDisplayMetrics().widthPixels / 100) * 24;
+                mImageView.setImageBitmap(result);
+            }
+        }
     }
+
+
 }
--- app/src/main/java/com/cpe327/piko/MainActivity.java
+++ app/src/main/java/com/cpe327/piko/MainActivity.java
@@ -103,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d("FACELOC", "signInWithCredential:success");
+//                            FirebaseUser user = mAuth.getCurrentUser();
                             updateUI();
                         } else {
                             // If sign in fails, display a message to the user.
@@ -114,7 +115,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
-
 
 }
 
